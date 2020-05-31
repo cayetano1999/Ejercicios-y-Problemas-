04c9094741be16cf916085e8430ddf2766f24a8c
CayetanoMarquez <josuecayetano@hotmail.com>
2020-05-31 18:48:27 -0400

    finish

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ namespace Algoritmos
     //Main Class
     public class Algoritm
     {
-        static List<int> numbersrandom = new List<int>();
         public static void Main(string[] args)
         {
             var numbersrandom = Utility.GenerateNumbers(1, 1000);
@@ -15,7 +14,7 @@ namespace Algoritmos
             var option = "";
 
             Console.WriteLine("------------------[Analisis y Diseño de Algoritmos]------------------");
-            for (int i = 1; i < 5; i++)
+            for (int i = 1; i < 7; i++)
             {
                 Console.WriteLine($"{i}. Modificacion {i}");
             }
@@ -38,6 +37,10 @@ namespace Algoritmos
 
                 case "4":
                     modification.ModificationFour(numbersrandom);
+                    break;
+
+                case "5":
+                    modification.ModificationFive(numbersrandom);
                     break;
 
 
@@ -126,6 +129,19 @@ namespace Algoritmos
             int mid = size / 2;
             double median = (size % 2 != 0) ? (double)sortedPNumbers[mid] : ((double)sortedPNumbers[mid] + (double)sortedPNumbers[mid - 1]) / 2;
             return median;
+        }
+
+        public static List<int> GetLastItems (List<int> values, int count, int range)
+        {
+            List<int> ultimoscien = new List<int>();
+
+            for (int i = 998; i >= 900; i--)
+            {
+                var item = values[i];
+                ultimoscien.Add(item);
+            }
+            var x = ultimoscien;
+            return x;
         }
 
         public static void ClearPromgram()
@@ -240,9 +256,10 @@ namespace Algoritmos
             Console.WriteLine("Considerar que se desea mostrar el producto de los logaritmos naturales de los 100 primeros números, \n si éste es mayor que la mediana de los 300 últimos números introducidos.");
 
 
-            var valuestaked = values.Take(300).ToList(); //Tomando los 300 primeros numeros
-            var logaritmos = new List<double>();
-            var mediana = Utility.GetMedian(valuestaked); // obteniendo la mediana de los primeros 300
+            var valuestaked = values.OrderByDescending(x=>x).ToList(); // Ordenado de forma descendente
+            valuestaked = valuestaked.Take(300).ToList();//Tomando los 300 ultimos numeros
+            var logaritmos = new List<double>(); // lista donde se guardaran los resultados de los logaritmos
+            var mediana = Utility.GetMedian(valuestaked); // obteniendo la mediana de los ultimos 300
 
             //Obteniendo de los 100 primeros numeros
             var first_hundred = values.Take(100).ToList();
@@ -266,6 +283,98 @@ namespace Algoritmos
             }
 
             Utility.ClearPromgram();
+
+        }
+
+        public void ModificationFive(List<int> values)
+        {
+            Console.Clear();
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("Considerar que se desea mostrar la raíz cúbica de la suma de los 100 primeros números introducidos,\n si esta es mayor que la raíz cuadrada del producto de los últimos 100 números introducidos.");
+            Console.WriteLine("\n");
+            //Tomando los  100 primeros de la lista
+            var valuestaked = values.Take(100).ToList();
+            //sumando los 100 numeros tomados
+            var suma = Utility.GetSum(valuestaked);
+            //obteniendo la raiz cubica del resultado de la suma
+            var raizcubica = Math.Pow(suma, (double)1 / 3);
+            //obteniendo los 100 ultimos numeros intriducidos
+            var ultimoscien = Utility.GetLastItems(values, values.Count, 900);
+            //obteniendo el producto de los 100 ultimos
+            var resultproducto = Utility.GetMultiplicaction(ultimoscien);
+            //obteniendo la raiz cuadrada del resultado del producto
+            var raizcuadrada = Math.Sqrt(resultproducto);
+
+            if (raizcubica > raizcuadrada)
+            {
+                Console.WriteLine($"\nLa raiz cubica de los 100 primeros numeros es: {raizcubica}");
+            }
+
+            else
+            {
+                Console.WriteLine("\nLa raiz cubica de la suma no es mayor que el de la raiz cubica del producto");
+            }
+
+            Utility.ClearPromgram();
+
+
+        }
+
+        public void ModificationSix(List<int> values)
+        {
+            Console.Clear();
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("Considerar que se desea mostrar el promedio de los números pares y cuadrados perfectos,\n si este promedio es igual a la mediana de los 100 primeros números pares y múltiplos de 5");
+            Console.WriteLine("\n");
+
+            foreach (var item in values)
+            {
+                var sum = 0;
+                var promedio = 0;
+                var cantidad = 0;
+
+                var cuadrado = Math.Sqrt(item).ToString(); // es perfecto si el resultado de la raiz cuadrada da un numero entero
+                var isPerfect = cuadrado.Contains(".") ? false : true; //si contiene puntos significa que es un decimal
+                if (isPerfect && item%2==0)
+                {
+                    cantidad++;
+                    //sumando solo los cuadrados perfectos y pares;
+                    sum += item;
+                }
+
+                promedio = sum / cantidad;
+
+                //tomando los 100 primeros pares y multiplos de 5;
+                var paresymultiplos = new List<int>();
+
+                for (int i = 0; i < values.Count; i++)
+                {
+                    if (paresymultiplos.Count < 100)
+                    {
+                        //pares y multiplos de 5
+                        if(values[i]%2==0 && values[i] % 5 == 0)
+                        {
+                            paresymultiplos.Add(values[i]);
+                        }
+                    }
+                }
+
+                //Obtener la mediana de los 100 primeros numeros multiplos de 5 y pares
+                var mediana = Utility.GetMedian(paresymultiplos);
+
+                if (promedio == mediana)
+                {
+                    Console.WriteLine($"El promedio es: {promedio}");
+                }
+                else
+                {
+                    Console.WriteLine($"No se puede mostrar el proomedio, no es igual a la mediana");
+                }
+            }
+
+
+            Utility.ClearPromgram();
+
 
         }
     }
